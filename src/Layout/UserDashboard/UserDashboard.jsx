4dfca841e783a46cc16e4a81fbba04df59210e83
Jubayer-K <jubayerkhan21@gmail.com>
2024-06-05 12:13:16 +0600

    made dashboard sidebar responsive

--- src/Layout/UserDashboard/UserDashboard.jsx
+++ src/Layout/UserDashboard/UserDashboard.jsx
@@ -1,62 +1,123 @@
+import { useState, useRef, useEffect } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { MdOutlineLibraryAdd } from "react-icons/md";
 import { TfiLayoutListPost } from "react-icons/tfi";
 import { GoHome } from "react-icons/go";
+
 const UserDashboard = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const sidebarRef = useRef(null);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+      setIsSidebarOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isSidebarOpen) {
+      document.addEventListener("click", handleClickOutside, true);
+    } else {
+      document.removeEventListener("click", handleClickOutside, true);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [isSidebarOpen]);
+
   return (
     <>
-      <div className="flex">
-        <div className="flex">
-          <aside className="flex flex-col w-64 h-screen px-4 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
-            <div className="flex flex-col items-center mt-6 -mx-2">
-              <img
-                className="object-cover w-24 h-24 mx-2 rounded-full"
-                src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-                alt="avatar"
-              />
-              <h4 className="mx-2 mt-2 font-medium text-gray-800 dark:text-gray-200">
-                John Doe
-              </h4>
-              <p className="mx-2 mt-1 text-sm font-medium text-gray-600 dark:text-gray-400">
-                john@example.com
-              </p>
-            </div>
+      <div className="flex h-screen">
+        <aside
+          ref={sidebarRef}
+          className={`flex z-50 flex-col w-64 h-full bg-white border-r dark:bg-gray-900 dark:border-gray-700 transition-transform transform ${
+            isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+          } md:translate-x-0 md:static fixed`}
+        >
+          <div className="flex flex-col items-center mt-6 -mx-2">
+            <img
+              className="object-cover w-24 h-24 mx-2 rounded-full"
+              src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+              alt="avatar"
+            />
+            <h4 className="mx-2 mt-2 font-medium text-gray-800 dark:text-gray-200">
+              John Doe
+            </h4>
+            <p className="mx-2 mt-1 text-sm font-medium text-gray-600 dark:text-gray-400">
+              john@example.com
+            </p>
+          </div>
 
-            <nav>
-              <ul>
-                <Link className="w-full" to={"/"}>
-                  <li className="flex btn items-center border-none hover:bg-gray-200">
-                    <GoHome></GoHome>
-                    <span className="mx-4 font-medium">Home</span>
-                  </li>
-                </Link>
-                <Link className="w-full"  to={'my-profile'}>
-                  <li className="flex btn items-center border-none hover:bg-gray-200">
-                    <CgProfile></CgProfile>
-                    <span className="mx-4 font-medium">My Profile</span>
-                  </li>
-                </Link>
-                <Link className="w-full" to={"add-post"}>
-                  <li className="flex btn items-center border-none hover:bg-gray-200">
-                    <MdOutlineLibraryAdd></MdOutlineLibraryAdd>
-                    <span className="mx-4 font-medium">Add Post</span>
-                  </li>
-                </Link>
+          <nav className="mt-10">
+            <ul className="dark:text-white">
+              <Link className="w-full" to="/">
+                <li className="flex btn items-center border-none hover:bg-gray-200 dark:hover:text-black">
+                  <GoHome />
+                  <span className="mx-4 font-medium">Home</span>
+                </li>
+              </Link>
+              <Link className="w-full" to="my-profile">
+                <li className="flex btn items-center border-none hover:bg-gray-200 dark:hover:text-black">
+                  <CgProfile />
+                  <span className="mx-4 font-medium">My Profile</span>
+                </li>
+              </Link>
+              <Link className="w-full" to="add-post">
+                <li className="flex btn items-center border-none hover:bg-gray-200 dark:hover:text-black">
+                  <MdOutlineLibraryAdd />
+                  <span className="mx-4 font-medium">Add Post</span>
+                </li>
+              </Link>
+              <Link className="w-full" to="my-posts">
+                <li className="flex btn items-center border-none hover:bg-gray-200 dark:hover:text-black">
+                  <TfiLayoutListPost />
+                  <span className="mx-4 font-medium">My Posts</span>
+                </li>
+              </Link>
+            </ul>
+          </nav>
+        </aside>
 
-                <Link className="w-full" to={"my-posts"}>
-                  <li className="flex btn items-center border-none hover:bg-gray-200">
-                    <TfiLayoutListPost></TfiLayoutListPost>
-
-                    <span className="mx-4 font-medium">My Posts</span>
-                  </li>
-                </Link>
-              </ul>
-            </nav>
-          </aside>
-        </div>
-        <div className="flex-1">
-          <Outlet></Outlet>
+        <div className="flex-1 flex flex-col">
+          <header className="flex items-center justify-between p-4 bg-white dark:bg-gray-900 border-b dark:border-gray-700 md:hidden">
+            <button
+              onClick={toggleSidebar}
+              className="text-gray-500 dark:text-gray-300 focus:outline-none"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {isSidebarOpen ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                )}
+              </svg>
+            </button>
+          </header>
+          <div className="flex-1 p-4">
+            <Outlet />
+          </div>
         </div>
       </div>
     </>
--- src/Pages/MyProfile/MyProfile.jsx
+++ src/Pages/MyProfile/MyProfile.jsx
@@ -1,10 +1,36 @@
+import PostCard from "../Shared/PostCard/PostCard";
 
 const MyProfile = () => {
-    return (
-        <>
-            <div>My profile page</div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <div className="flex flex-col items-center p-8 transition-colors duration-300 transform cursor-pointer group hover:bg-blue-600 rounded-xl">
+          <img
+            className="object-cover w-32 h-32 rounded-full ring-4 ring-gray-300"
+            src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+            alt="Amelia Anderson Profile Picture"
+          />
+          <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white group-hover:text-white">
+            Amelia Anderson
+          </h1>
+          <p className="mt-2 text-gray-500 capitalize dark:text-gray-300 group-hover:text-gray-300">
+            Lead Developer
+          </p>
+            <div>
+                Badge
+            </div>
+        </div>
+        <h1 className="text-2xl my-6 mt-9 font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">
+          Recent <span className="text-blue-500 ">Posts</span>
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <PostCard></PostCard>
+          <PostCard></PostCard>
+          <PostCard></PostCard>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MyProfile;
